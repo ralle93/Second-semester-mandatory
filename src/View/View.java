9129e4f9cb837d08fc771ee1b2adb9817fba2dde
Tommy Troest <tommytroest@hotmail.com>
2017-03-31 12:31:06 +0200

    Login GUI Basics Done

--- src/View/View.java
+++ src/View/View.java
@@ -1,10 +1,14 @@
 package View;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -17,15 +21,32 @@ public class View {
         Label loginLabel = new Label("LOGIN");
         Label userIdLabel = new Label("User ID:");
         Label userPassword = new Label("User Password");
+        Button quitButton = new Button("Quit");
+        Button loginButton = new Button("Login");
 
         TextField userIdField = new TextField();
+        PasswordField passwordField = new PasswordField();
+
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setPadding(new Insets(10,10,10,10));
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+        gridPane.add(userIdLabel, 0, 0);
+        gridPane.add(userIdField, 1, 0);
+        gridPane.add(userPassword, 0,1);
+        gridPane.add(passwordField, 1,1);
 
         HBox hbox = new HBox();
-        hbox.getChildren().add(userIdLabel);
-        hbox.getChildren().add(userIdField);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.getChildren().add(quitButton);
+        hbox.getChildren().add(loginButton);
 
         VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(25);
         vbox.getChildren().add(loginLabel);
+        vbox.getChildren().add(gridPane);
         vbox.getChildren().add(hbox);
         Scene loginScene = new Scene(vbox, 1000, 800);
         return loginScene;

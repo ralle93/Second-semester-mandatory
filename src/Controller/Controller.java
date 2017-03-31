c5007df4765335cc179ec22690ae3834d88b26f5
Søren Skovgaard <soren.ds@gmail.com>
2017-03-31 12:18:56 +0200

    git gud

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,11 @@
 package Controller;
 
+import Model.DataLayer;
+
 public class Controller {
+   private DataLayer dataLayer;
+
+   public Controller(DataLayer dataLayer) {
+      this.dataLayer = dataLayer;
+   }
 }

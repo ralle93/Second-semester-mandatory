5c79ead32fc1ac2c1cef89c5b0900d6d094d970f
Rasmus Thrane <snubi68@gmail.com>
2017-03-31 12:17:08 +0200

    blabla

--- src/Model/DataLayer.java
+++ src/Model/DataLayer.java
@@ -31,7 +31,6 @@ public class DataLayer {
 
 
       return t;
-
    }
 
    public static boolean fetchUser(String user, String pass) {
@@ -40,3 +39,54 @@ public class DataLayer {
 
 
 }
+
+/*
+package mysqlPractice;
+
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.text.*;
+import java.util.Calendar;
+
+/**
+ * Created by rasmusthrane on 12/01/17.
+
+public class sqltest {
+
+
+   public static void main(String[] args) {
+      String dbURL = "jdbc:mysql://localhost:3306/database?useSSL=false";
+      String username = "root";
+      String password = "kun4cj";
+
+      String query = "SELECT * FROM  loginform";
+
+      try {
+         Connection connection = DriverManager.getConnection(dbURL, username, password);
+
+         Calendar calender = Calendar.getInstance();
+         java.sql.Date startDate = new java.sql.Date(calender.getTime().getTime());
+
+         Statement stmt = connection.createStatement();
+
+         ResultSet rs = stmt.executeQuery(query);
+         System.out.println(rs.toString());
+         while(rs.next()){
+            String usern = rs.getObject(1).toString();
+            String pass = rs.getObject(2).toString();
+
+            System.out.println("username = " + usern + "  password = " + pass);
+
+         }
+
+         connection.close();
+
+      } catch (SQLException e) {
+         System.out.println(e);
+      }
+   }
+}
+
+
+
+ */

d2faf5ddc9d53c7692b76c7614695cd7531f29ec
Rasmus Thrane <snubi68@gmail.com>
2017-03-31 10:04:06 +0200

    thannni tarn

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import Model.DataLayer;
+
+/**
+ * Created by rasmusthrane on 31/03/17.
+ */
+public class Main {
+
+   public static void main(String[] args) {
+
+      DataLayer d = new DataLayer();
+
+      d.connectToDb();
+   }
+}
--- src/Model/DataLayer.java
+++ src/Model/DataLayer.java
@@ -1,9 +1,25 @@
 package Model;
 
+import java.sql.*;
+
 /**
  * Created by rasmusthrane on 31/03/17.
  */
 public class DataLayer {
 
+   public void connectToDb(){
+      String dbURL = "jdbc:mysql://sql8.freemysqlhosting.net:3306/sql8166696?useSSL=false";
+      String username = "sql8166696";
+      String password = "Lp7AZi7fVf";
 
+      String query = "SELECT * FROM  loginform";
+
+      try {
+         Connection connection = DriverManager.getConnection(dbURL, username, password);
+
+         System.out.println(connection);
+      } catch (SQLException e) {
+         System.out.println(e);
+      }
+   }
 }

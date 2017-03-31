cee320931f0798ff8af3a0c5308733b0c2c3694d
Tommy Troest <tommytroest@hotmail.com>
2017-03-31 11:04:44 +0200

    third GUI commit

--- src/View/View.java
+++ src/View/View.java
@@ -1,16 +1,24 @@
 package View;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
 /**
  * Tommy is primary contributor for this class.
  */
 public class View {
 
-    public static void loginView() {
-
+    public static Scene loginView() {
+        VBox vbox = new VBox();
+        Scene loginScene = new Scene(vbox, 1000, 800);
+        return loginScene;
     }
 
-    public static void mainView() {
-
+    public static Scene mainView() {
+        BorderPane borderpane = new BorderPane();
+        Scene mainScene = new Scene(borderpane, 1000, 800);
+        return mainScene;
     }
 
     public static void inventoryView() {
